824599d1576bb380d9aa5d21d904d9b4148a3ed6
Sirshak Bohara <boharasirshak101@gmail.com>
2024-01-07 13:13:17 +0545

    Add payment email template and configure email functionality

--- electropuzzle-backend/src/index.js
+++ electropuzzle-backend/src/index.js
@@ -1,11 +1,26 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const { transporter, sendEmail } = require('./lib/email');
+
+dotenv.config();
+
+const PORT = process.env.PORT || 3000;
 const app = express();
 
-app.get('/', (req, res) => {
+app.use(express.json());
+
+transporter.verify((error, success) => {
+    if (error) {
+      throw error;
+    } else {
+      console.log("Server is ready to send messages");
+    }
+  });
+
+app.get('/', async (req, res) => {
     res.send('Hello, World!');
 });
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
     console.log('Server is running on port 3000');
 });
