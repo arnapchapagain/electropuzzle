406368c0f7800b8f962b3226e5ba487dfef1ca95
Sirshak Bohara <boharasirshak101@gmail.com>
2024-01-09 23:48:34 +0545

    Refactored the order controller by
    - Modularizing the code into small functions
    - Automitically calculating prices of items
    - Ignoring negative prices.
    - Not letting one promo code to be used twice.

--- electropuzzle-strapi/src/api/order/controllers/order.ts
+++ electropuzzle-strapi/src/api/order/controllers/order.ts
@@ -2,33 +2,57 @@
  * order controller
  */
 
-import { factories } from '@strapi/strapi'
-import { YooCheckout, ICreatePayment } from '@a2seven/yoo-checkout';
-import Joi from 'joi';
+import { factories } from "@strapi/strapi";
+import { YooCheckout, ICreatePayment } from "@a2seven/yoo-checkout";
+import Joi from "joi";
+import { Context } from "koa";
 
-const checkout = new YooCheckout({ 
-    shopId: process.env.SHOP_ID || "your_shop_id", 
-    secretKey: process.env.SECRET_KEY || "your_secret_key" 
+const checkout = new YooCheckout({
+  shopId: process.env.SHOP_ID || "your_shop_id",
+  secretKey: process.env.SECRET_KEY || "your_secret_key",
 });
 
-function genV4UUID() {
-    /**
-     * A basic v4 UUID generator. It's not completely "safe enough" as it's using Math.random(), however it's good enough for now.
-     * There is a chance of collision, but it's very low, like 2^-64 or something.
-     */
-    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-      // generate a random number between 0 and 15 and convert it to base 16.  
-      const b64 = (Math.random() * 16) | 0;
-      // since we're using v4 UUID, we need to set the 2 most significant bits (bits 6 and 7) of the 7th byte to 0 and 1, respectively.
-      const uuid4 = c == 'x' ? b64 : (b64 & 0x3) | 0x8; 
-      // convert to string in base 16 and return.
-      return uuid4.toString(16);
-    });
-}
+// This is a constant that is used to ensure idempotency of the payment creation request.
+const IDEMPOTENCY_KEY = genV4UUID();
+
+export default factories.createCoreController(
+  "api::order.order",
+  ({ strapi }) => ({
+    async create(ctx: Context) {
+      await this.validateQuery(ctx);
+      const validationResult = validateRequestBody(ctx);
+      if (validationResult.error) return ctx.badRequest(validationResult.error);
+      const body: ICreateOrder = validationResult.value;
+
+      const basket = await getBasket(body.basket_id);
+      if (!basket) return ctx.badRequest("Basket not found");
+      if (basket.pedals.length === 0) return ctx.badRequest("Basket is empty");
+
+      try {
+        let totalCost = await calculateTotalCost(basket.pedals, body.promo_codes);
+
+        body.status = 'not_completed';
+        const orderId = await createOrderEntry(body);
+
+        const payload = createPaymentPayload(totalCost.toString(), orderId.toString());
+
+        const paymentInfo = await processPayment(payload, orderId);
+        return this.transformResponse(paymentInfo);
+
+      } catch (error) {
+          ctx.badRequest(error);
+      }
+    },
+
+    async success(ctx) {
+      ctx.body = "success";
+      console.log(ctx.request.query);
+    },
+  }),
+);
 
 const createOrderInterface = Joi.object({
   basket_id: Joi.number().required(),
-  total: Joi.number().required(),
   full_name: Joi.string().required(),
   phone: Joi.string().required(),
   email: Joi.string().email().required(),
@@ -39,12 +63,12 @@ const createOrderInterface = Joi.object({
   address_floor: Joi.string(),
   address_entrance: Joi.string(),
   comments: Joi.string(),
-  payment_id: Joi.string()
+  payment_id: Joi.string(),
+  promo_codes: Joi.array().items(Joi.string()),
 });
 
-interface ICreateOrderInterface {
+interface ICreateOrder {
   basket_id: number;
-  total: number;
   full_name: string;
   phone: string;
   email: string;
@@ -56,80 +80,113 @@ interface ICreateOrderInterface {
   address_entrance?: string;
   comments?: string;
   payment_id?: string;
+  promo_codes?: string[];
 }
 
-// This is a constant that is used to ensure idempotency of the payment creation request.
-const IDEMPOTENCY_KEY = genV4UUID();
+function genV4UUID() {
+  /**
+   * A basic v4 UUID generator. It uses Math.random().
+   * There is a chance of collision, but it's very low, like 2^-64 or something.
+   */
+  return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function (c) {
+    const b64 = (Math.random() * 16) | 0;
+    const uuid4 = c == "x" ? b64 : (b64 & 0x3) | 0x8;
+    return uuid4.toString(16);
+  });
+}
 
-export default factories.createCoreController(
-    "api::order.order",
-    ({ strapi }) => ({
-  
-      async create(ctx) {
+function validateRequestBody(ctx) {
+  const { error, value } = createOrderInterface.validate(ctx.request.body);
+  return { error, value: value as ICreateOrder };
+}
 
-        await this.validateQuery(ctx);
+async function getBasket(basketId: number) {
+  return await strapi.db.query("api::basket.basket").findOne({
+      where: { id: basketId },
+      populate: true,
+  });
+}
 
-        // Validate request body
-        const { error, value } = createOrderInterface.validate(ctx.request.body);
-        if (error) return ctx.badRequest(error)
-        
-        const body = value as ICreateOrderInterface;
+// Fetches pedal information from the database
+async function fetchPedalInfo(pedalId: string) {
+  return await strapi.db.query("api::pedal.pedal").findOne({
+    where: { id: pedalId },
+    populate: true,
+  });
+}
 
-        // check if the basket_id provived by user is valid
-        const basket = await strapi.db.query('api::basket.basket').findOne({
-          select: ['*'],
-          where: { id: body.basket_id },
-          populate: { category: true },
-        });
-        if (!basket) return ctx.badRequest('Basket not found');
+// Calculates the price of a pedal after applying applicable promo codes
+function calculateDiscountedPrice(pedalInfo: any, promoCodes: string[]) {
+  let price = pedalInfo.price;
+  pedalInfo.promo_codes.forEach(applicablePromoCode => {
+    if (promoCodes.includes(applicablePromoCode.code)) {
+      let discountAmount = pedalInfo.price * (applicablePromoCode.discount_percentage / 100);
+      price -= discountAmount;
+      if (price <= 0) price = 0; // Prevents negative prices
+    }
+  });
+  return price;
+}
 
-        body.status = 'not_completed';
-        const entry = await strapi.entityService.create('api::order.order', {
-          data: {
-            ...body
-          }
-        });
-        let orderId = entry.id;
+// Determines the final price of a pedal, considering whether promo codes are applied or not
+async function calculatePedalPrice(pedal: any, promoCodes: string[]) {
+  const pedalInfo = await fetchPedalInfo(pedal.id);
+  if (!pedalInfo) {
+    throw new Error(`Pedal with ID ${pedal.id} not found in the database`);
+  }
 
-        const payload: ICreatePayment = {
-            amount: {
-                value: body.total.toString(),
-                currency: 'RUB'
-            },
-            payment_method_data: {
-                type: 'bank_card'
-            },
-            confirmation: {
-                type: 'redirect',
-                return_url: process.env.CHECKOUT_SUCCESS_URL || 'http://localhost:1337/orders/success?order_id=' + orderId
-            }
-        };
-        
-        try {
-            const payment = await checkout.createPayment(payload, IDEMPOTENCY_KEY);
+  return promoCodes.length > 0 ? calculateDiscountedPrice(pedalInfo, promoCodes) : pedalInfo.price;
+}
 
-            // update order with payment_id and status
-            await strapi.entityService.update('api::order.order', orderId, {
-              data: {
-                payment_id: payment.id,
-                status: 'pending'
-              },
-            });
+// Iterates over all pedals to calculate the total cost
+async function calculateTotalCost(pedals: any[], promoCodes: string[]) {
+  let totalCost = 0;
+  if (!promoCodes) promoCodes = [];
 
-            // return order_id and payment_info
-            return this.transformResponse({
-              order_id: orderId,
-              payment_info: payment
-            });
+  // remove all duplicates from promoCodes so that we don't apply the same promo code twice
+  promoCodes = [...new Set(promoCodes)];
 
-        } catch (error) {
-             ctx.badRequest(error)
-        }
+  for (const pedal of pedals) {
+    const pedalPrice = await calculatePedalPrice(pedal, promoCodes);
+    totalCost += pedalPrice;
+  }
+
+  return totalCost;
+}
+
+async function createOrderEntry(orderData: ICreateOrder) {
+  const entry = await strapi.entityService.create('api::order.order', {
+      data: { ...orderData }
+  });
+  return entry.id;
+}
+
+function createPaymentPayload(total: string, orderId: string) {
+  return {
+    amount: {
+        value: total,
+        currency: 'RUB'
+    },
+    payment_method_data: {
+        type: 'bank_card'
+    },
+    confirmation: {
+        type: 'redirect',
+        return_url: process.env.CHECKOUT_SUCCESS_URL || 'http://localhost:1337/orders/success?order_id=' + orderId
+    }
+  } as ICreatePayment;
+}
+
+async function processPayment(payload, orderId) {
+  let payment = await checkout.createPayment(payload, IDEMPOTENCY_KEY);
+  await strapi.entityService.update('api::order.order', orderId, {
+      data: {
+          payment_id: payment.id,
+          status: 'pending'
       },
-
-      async success(ctx) {
-        ctx.body = 'success';
-        console.log(ctx.request.query);
-      }
-    })
-  );
+  });
+  return {
+      order_id: orderId,
+      payment_info: payment
+  };
+}
