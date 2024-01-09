431a3c174e4038d60cee6071d4e9772ee493dc14
bibekshhh <bibekrock2019@gmail.com>
2024-01-09 17:52:54 +0545

    fix ui of products page

--- electropuzzle-next/app/pedals/page.tsx
+++ electropuzzle-next/app/pedals/page.tsx
@@ -11,64 +11,190 @@ const productsList = {
       "data": [
         {
           "attributes": {
-            "name": "Pedal",
-            "category": "Overdrive",
-            "based_on": "gg",
-            "price": 55,
+            "name": "Modi Ji",
+            "based_on": "Indian Government",
+            "price": 69420,
             "stock": 1,
-            "description": "ff",
+            "description": "# Modi Ji \n## The Prime Minister of India\n\nAayega toh modi hi 🔥🔥\nDekh kya raha hai aaise 😡? Modi ji mere bhagwan hai. 😍😘🥰\nOni cha. Use code **bhen-k-lodey** for 69% off 😏🤤🥵🥵",
             "image": {
               "data": {
                 "attributes": {
-                  "name": "electro-puzzle-logo.png",
-                  "url": "/uploads/electro_puzzle_logo_97baf571cd.png",
+                  "name": "Untitled.jpeg",
+                  "url": "/uploads/Untitled_7e5d6e6564.jpeg",
                   "alternativeText": null
                 }
               }
             },
-            "additional_imgages": {
+            "additional_images": {
               "data": []
             },
-            "peculiarity": null,
-            "management": null,
-            "characteristics": null,
-            "promo_codes": {
+            "peculiarity": "Modi ji is more pecular than the pecular word itself. Be careful what you say about him. 2024 mi aayega toh modi hi ",
+            "management": {
+              "first_name": "Modi",
+              "last_name": "Ji"
+            },
+            "characteristics": {
+              "chest": "Very big. 46+ inch",
+              "bicep": "Arnold ke papa"
+            },
+            "slug": "modi-ji",
+            "videos": {
               "data": []
+            },
+            "promo_codes": {
+              "data": [
+                {
+                  "attributes": {
+                    "code": "bhed-k-lodey",
+                    "discount_percentage": 69,
+                    "expiration_date": "2025-01-31"
+                  }
+                },
+                {
+                  "attributes": {
+                    "code": "modi-ji-hot-pics",
+                    "discount_percentage": 100,
+                    "expiration_date": "2023-01-31"
+                  }
+                },
+                {
+                  "attributes": {
+                    "code": "no-barganing",
+                    "discount_percentage": 0,
+                    "expiration_date": "2025-01-31"
+                  }
+                },
+                {
+                  "attributes": {
+                    "code": "rajeshDai",
+                    "discount_percentage": 100,
+                    "expiration_date": "2039-12-31"
+                  }
+                }
+              ]
+            },
+            "categories": {
+              "data": [
+                {
+                  "attributes": {
+                    "type": "Series",
+                    "value": "Black series",
+                    "description": null
+                  }
+                },
+                {
+                  "attributes": {
+                    "type": "Main",
+                    "value": "Overdrive",
+                    "description": null
+                  }
+                }
+              ]
             }
           }
         },
         {
           "attributes": {
-            "name": "gg",
-            "category": "Overdrive",
-            "based_on": "gg",
-            "price": 500,
-            "stock": 30,
-            "description": "gg",
+            "name": "Rajesh Dai",
+            "based_on": "rajesh dai",
+            "price": 1000000,
+            "stock": 10000000,
+            "description": "# Rajesh Dai \n## K sochya bhai, yo ho rajesh dai 🔥🔥\n\nDharahara bhaneko k ho taha xa? **Rajesh dai ko L**\nRajesh dai jasto omnipotent manxe bheteko xas ?\n- Yeti barsa bhayo, still he is our dai 🥰🥰\n\nRajesh dai lai ma yeti maya garchu\n\n```c\nwhile (true){\n    printf(\"I love you rajesh dai x%d\", ++i);\n}\n```\n",
             "image": {
               "data": {
                 "attributes": {
-                  "name": "electro-puzzle-logo.png",
-                  "url": "/uploads/electro_puzzle_logo_97baf571cd.png",
-                  "alternativeText": null
+                  "name": "images.jpeg",
+                  "url": "/uploads/images_70ccd31548.jpeg",
+                  "alternativeText": "Rajesh dai kungfu panda"
                 }
               }
             },
-            "additional_imgages": {
+            "additional_images": {
+              "data": [
+                {
+                  "attributes": {
+                    "name": "images.jpeg",
+                    "url": "/uploads/images_c85d3d9466.jpeg",
+                    "alternativeText": "Rajesh dai never cuts"
+                  }
+                },
+                {
+                  "attributes": {
+                    "name": "images.jpeg",
+                    "url": "/uploads/images_8d238b2351.jpeg",
+                    "alternativeText": "Breaking news"
+                  }
+                },
+                {
+                  "attributes": {
+                    "name": "images.jpeg",
+                    "url": "/uploads/images_674b14c816.jpeg",
+                    "alternativeText": "Rajesh dai's emails"
+                  }
+                },
+                {
+                  "attributes": {
+                    "name": "images.jpeg",
+                    "url": "/uploads/images_6251d64243.jpeg",
+                    "alternativeText": "Rajesh dai horni looks"
+                  }
+                },
+                {
+                  "attributes": {
+                    "name": "images.jpeg",
+                    "url": "/uploads/images_398b4db655.jpeg",
+                    "alternativeText": "Rajesh dai power"
+                  }
+                }
+              ]
+            },
+            "peculiarity": "Rajesh dia is everywhere. I see him in mobile, computer, google, youtube, my bed, toilet, my eyes. uff",
+            "management": {
+              "manager": "rajesh dai doesn't need manager. he manages nepal",
+              "residence": "rajesh dai lives everywhere"
+            },
+            "characteristics": {
+              "feature": "features lekhdai gayo bhaney ta computer ko memory sakki jala."
+            },
+            "slug": "rajesh-dai",
+            "videos": {
               "data": []
             },
-            "peculiarity": null,
-            "management": null,
-            "characteristics": null,
             "promo_codes": {
-              "data": []
+              "data": [
+                {
+                  "attributes": {
+                    "code": "rajeshDai",
+                    "discount_percentage": 100,
+                    "expiration_date": "2039-12-31"
+                  }
+                }
+              ]
+            },
+            "categories": {
+              "data": [
+                {
+                  "attributes": {
+                    "type": "Series",
+                    "value": "Gold series",
+                    "description": null
+                  }
+                },
+                {
+                  "attributes": {
+                    "type": "Main",
+                    "value": "Boosters",
+                    "description": null
+                  }
+                }
+              ]
             }
           }
         }
       ]
     }
   }
-};
+}
 
 export default function Pedals() {
     return (
